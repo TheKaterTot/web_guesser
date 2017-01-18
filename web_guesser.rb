de9ab6838cb92665cb2fc393585dc8891e3eb12e
Katy Farmer <dreadpiratekaty@gmail.com>
2017-01-17 21:16:54 -0700

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,8 +5,32 @@ def random
   rand(100)
 end
 
-number = random
+SECRET_NUMBER = random
 
 get "/" do
-  erb :index, :locals => {:number => number}
+  erb :index, :locals => {:message => ""}
+end
+
+post "/" do
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:message => message}
+end
+
+def check_guess(guess)
+  if check_difference(guess, SECRET_NUMBER)
+    "Way too high!"
+  elsif guess > SECRET_NUMBER
+    "Too high!"
+  elsif check_difference(SECRET_NUMBER, guess)
+    "Way too low!"
+  elsif guess < SECRET_NUMBER
+    "Too low!"
+  else
+    "You got it right! The number is #{number}."
+  end
+end
+
+def check_difference(num1, num2)
+  num1 - num2 > 5
 end
